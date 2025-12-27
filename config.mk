40cb93eb774f076372017f222ee474264c9c8b69
Zed Kaido <z@zedkaido.com>
2025-12-27 15:34:32 +0000

    [config.mk] add macOS build support

--- config.mk
+++ config.mk
@@ -7,8 +7,25 @@ VERSION = 6.6
 PREFIX = /usr/local
 MANPREFIX = ${PREFIX}/share/man
 
-X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
+OS := $(shell uname -s)
+ifeq ($(OS),Linux)
+	X11INC = /usr/X11R6/include
+	X11LIB = /usr/X11R6/lib
+else ifeq($(OS),Darwin)
+	X11INC = /opt/X11/include
+	X11LIB = /opt/X11/lib
+	BREW_FREETYPE = $(shell brew --prefix freetype 2>/dev/null || echo "usr/local/bin/freetype")
+	FREETYPEINC = $(BREW_FREETYPE)/include/freetype2
+else ifeq($(OS),OpenBSD)
+	X11INC = /usr/X11R6/include
+	X11LIB = /usr/X11R6/lib
+	FREETYPEINC = $(X11INC)/freetype2
+	MANPREFIX = ${PREFIX}/man
+else
+	$(WARNING: Unsupported OS: $(OS), using linux defaults)
+	X11INC = /usr/X11R6/include
+	X11LIB = /usr/X11R6/lib
+endif
 
 # Xinerama, comment if you don't want it
 XINERAMALIBS  = -lXinerama
@@ -16,10 +33,6 @@ XINERAMAFLAGS = -DXINERAMA
 
 # freetype
 FREETYPELIBS = -lfontconfig -lXft
-FREETYPEINC = /usr/include/freetype2
-# OpenBSD (uncomment)
-#FREETYPEINC = ${X11INC}/freetype2
-#MANPREFIX = ${PREFIX}/man
 
 # includes and libs
 INCS = -I${X11INC} -I${FREETYPEINC}
@@ -27,7 +40,6 @@ LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} ${FREETYPELIBS}
 
 # flags
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700L -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
-#CFLAGS   = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
 CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Os ${INCS} ${CPPFLAGS}
 LDFLAGS  = ${LIBS}
 
